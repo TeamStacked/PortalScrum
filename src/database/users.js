1ca8e20bc0831d26c9be1298c29dbbffa50ed969
Vinicius Guin <viniciusguin7@gmail.com>
2026-04-27 22:16:11 -0300

    Feat(#32): "Modifiquei alterações de outra branch"

--- src/database/users.js
+++ src/database/users.js
@@ -1,8 +1,8 @@
 const pool = require("./db");
 
 
-async function cadatrarUsuarioBD(cpf, name, email, senha, hash) {
-    const sql = `insert into usuario(cpf_usuario,nome_usuario,email_usuario, senha_usuario, certificado_hash) values ($1, $2, $3, $4, $5) returning *`;
+async function cadatrarUsuarioBD(cpf, name, email, senha) {
+    const sql = `insert into usuario(cpf_usuario,nome_usuario,email_usuario, senha_usuario, certificado_hash) values ($1, $2, $3, $4) returning *`;
     const values = [cpf, name, email, senha, hash];
     const {rows} = await pool.query(sql,values);
     return rows[0];
