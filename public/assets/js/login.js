13e64df1835593fafa2b034e9b025e94dc2ae198
Pedro <phjs7626@gmail.com>
2026-04-29 17:37:56 -0300

    feat(#17): fluxo completo de autenticação (cadastro, login, validação e redirect)

---
+++ public/assets/js/login.js
@@ -0,0 +1,31 @@
+form &&
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const valido = window.validarFormulario();
+
+        if (!valido) return;
+        const data = {
+            cpf: inputCPF.value,
+            senha: inputPassword.value,
+        };
+
+        const response = await fetch("http://localhost:3000/api/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        const result = await response.json();
+
+        if (!response.ok) {
+            alert(result.message);
+            return;
+        }
+
+        alert("Login realizado!");
+        localStorage.setItem("token", result.token);
+        window.location.href = "/home.html";
+    });
