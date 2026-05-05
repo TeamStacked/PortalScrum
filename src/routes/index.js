9c998b74fc546a800135244c3652c366dc2109af
portug4lucas <portugallucas43@gmail.com>
2026-05-05 19:42:52 -0300

    feat (#7): registrar rotas de exame no router principal

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
-const { Router, json } = require("express");
+const { Router } = require("express");
 const usuario = require("./usuario.routes");
 const auth = require("./auth.routes");
+const exame = require("./exame.routes");
 
 const router = Router();
 
 router.use("/usuarios", usuario);
 router.use("/auth", auth);
+router.use("/exames", exame);
 
 router.use(function (req, res) {
     res.status(404).json({ message: "Rota inexistente" });
 });
-router;
+
 module.exports = router;
