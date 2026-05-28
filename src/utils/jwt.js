50bb4b007691f418e006b7d7e3e63945ec5c5707
ThiagoDT <galaspion5td@gmail.com>
2026-05-27 23:02:43 -0300

    refactor(#119) adicionando cookies no JWT CreateToken

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -7,10 +7,13 @@ dotenv.config({
     path: path.resolve(__dirname, "..", "..", ".env"),
 });
 
-function createToken(payload) {
-    return jwt.sign(payload, process.env.JWT_SECRET, {
+function createToken(payload, res) {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: Number(process.env.DEFAULT_EXPIRES_IN_SECONDS),
     });
+    //O token apaga apos o fechamento do navegador
+    res.cookie('token',token, {httpOnly: true, secure: false, sameSite: 'strict', maxAge:process.env.DEFAULT_EXPIRES_IN_SECONDS*1000});
+    return token;
 }
 
 function verifyToken(token) {
